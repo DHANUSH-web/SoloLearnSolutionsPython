ffd5e53b6dd8a9e440b32a9a3dd63e92ba166968
DHANUSH H V <67680433+DHANUSH-web@users.noreply.github.com>
2021-01-03 10:11:19 +0530

    Add files via upload

---
+++ simpleCalculator.py
@@ -0,0 +1,21 @@
+'''
+Simple Calculator
+
+
+Write a program to take two integers as input and output their sum.
+
+Sample Input:
+2
+8
+
+Sample Output:
+10
+Remember, input() results in a string.
+'''
+def add(x, y):
+    return x+y
+
+x=int(input())
+y=int(input())
+
+print(add(x, y))
